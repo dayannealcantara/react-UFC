fb7b2e52c224d16591419c8e457f6a81d9ab3cc3
Dayanne Alcantara <dayannealcantara14@gmail.com>
2021-11-17 13:39:01 -0300

    criando aplicação

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 
 
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ 
+    <App />,
+
   document.getElementById('root')
 );
 
