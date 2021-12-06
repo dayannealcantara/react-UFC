e714f89cdff090937f517cd6ea308b3cab7c70cf
Dayanne Alcantara <dayannealcantara14@gmail.com>
2021-12-06 10:49:36 -0300

    Header finalizado

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 
 import './App.css';
+import {useState} from 'react';
+
+
 
 function App() {
+
+ const [valor, setValor] = useState()
+
+  const handleSubmit= (e) => {
+    e.preventDefault();  
+    setValor(parseFloat(e.target.number1.value) + parseFloat(e.target.number2.value));
+    console.log(e.target.number1.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <section className="aplicacao">
       <h1>Somando dois números naturais</h1>
+      <form onSubmit={handleSubmit}>
+      <div className="soma">      
+       <input type="text" className="app-number" placeholder="Número" name="number1"/>
+       
+       <input type="text" className="app-number" placeholder="Número" name="number2"/>
       
-      <div className="soma">
-       <input type="text" className="app-number" placeholder="Número"></input>
-       <br></br>
-       <input type="text" className="app-number" placeholder="Número"></input>
-      <br></br>
       </div>
-       <button className="app-button"> + </button>
-       <p className="resultado"> </p>
-       </header>
+       <button type="submit" className="app-button"> + </button>       
+       </form>
+       {valor && <h1>{valor}</h1>}
+       </section>
+  
     </div>
   );
 }
